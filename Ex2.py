e290678f648d74bd05751570cc7c608b6de95478
Natalia Kushnir <natalka7618@yandex.ru>
2022-07-11 14:22:24 +0300

    написана программа 2

--- Ex2.py
+++ Ex2.py
@@ -1,3 +1,11 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-# Пример:
-# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def multiplication(n):
+    return 1 if n == 0 else n * multiplication(n - 1)
+
+n = int(input('Введите число: '))
+i = 1
+while i <= n:
+    print (multiplication(i))
+    i += 1
+
