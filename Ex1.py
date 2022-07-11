1ff206609ed88223c9c95265e48c51b5b15a2db0
Natalia Kushnir <natalka7618@yandex.ru>
2022-07-11 13:26:15 +0300

    написана программа 1

--- Ex1.py
+++ Ex1.py
@@ -2,3 +2,12 @@
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
+
+n = float(input('Введите число: '))
+str_n = str(n)
+str_n = str_n.replace('.', '')
+list_str = list(str_n)
+list_num = map(int, list_str)
+s = sum (list_num)
+print(s)
+
